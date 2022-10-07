6e6caef23ca368fc72244df5b6488d4bb46f9569
Evan Hernandez <evanfredhernandez@gmail.com>
2022-10-06 20:07:42 -0400

    Add shell script.

---
+++ mediation/causal_trace.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+MODEL_NAME="gpt2"
+DEVICE=5
+
+for DATASET in counterfact winoventi
+do
+    for MEDIATED in med umed
+    do
+        for TARGET in subj_first subj_last attr
+        do
+            key=${DATASET}_${MEDIATION}_${TARGET}
+            CUDA_VISIBLE_DEVICES=$DEVICE python3 -m experiments.causal_trace \
+                --model_name $MODEL_NAME \
+                --fact_file data/mediation/${KEY}.json \
+                --output_dir results/{model_name}/${KEY}/causal_trace
+        done
+    done
+done
